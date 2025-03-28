ca97f8fb8854ce114687d9cb30df76a03df95421
henryak25 <henryadityakosasi@gmail.com>
2025-03-28 18:08:45 +0700

    initial commit

---
+++ src/main/java/id/ac/ui/cs/advprog/hiringgo/HiringGoApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.hiringgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiringGoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HiringGoApplication.class, args);
+    }
+
+}
