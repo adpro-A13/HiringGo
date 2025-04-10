8dfd5944af1f8b6a9f16227094428efe5bd45971
arshqiii <marshq23@gmail.com>
2025-04-11 06:37:30 +0700

    [RED] Created MataKuliahTest

---
+++ src/test/java/id/ac/ui/cs/advprog/hiringgo/model/MataKuliahTest.java
@@ -0,0 +1,58 @@
+package id.ac.ui.cs.advprog.hiringgo.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MataKuliahTest {
+
+    @Test
+    void testCreateMataKuliah_Success(){
+        MataKuliah mataKuliah = new MataKuliahBuilder()
+                .withKode("CSCM602023 - 01.00.12.01-2020")
+                .withNama("Pemrograman Lanjut")
+                .withDeskripsi("Membahas Java & Spring Boot")
+                .addDosenPengampu("Dosen A")
+                .addDosenPengampu("Dosen B")
+                .build();
+
+        assertEquals("CSCM602023 - 01.00.12.01-2020", mataKuliah.getKode());
+        assertEquals("Pemrograman Lanjut", mataKuliah.getNama());
+        assertEquals("Membahas Java & Spring Boot", mataKuliah.getDeskripsi());
+        assertEquals(2, mataKuliah.getDosenPengampu().size());
+    }
+
+    @Test
+    void testCreateMataKuliahWithoutNama() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new MataKuliahBuilder()
+                    .withKode("CSCM602023 - 01.00.12.01-2020")
+                    .withDeskripsi("Deskripsi")
+                    .addDosenPengampu("Dosen A")
+                    .build();
+        }, "Harap sertakan nama mata kuliah");
+    }
+
+
+    @Test
+    void testCreateMataKuliahWithoutKode(){
+        assertThrows(IllegalArgumentException.class, () -> {
+            MataKuliah mataKuliah = new MataKuliahBuilder()
+                    .withNama("Pemrograman Lanjut")
+                    .withDeskripsi("Deskripsi")
+                    .addDosenPengampu("Dosen A")
+                    .build();
+        }, "Kode Mata Kuliah harus diisi");
+    }
+
+    @Test
+    void testCreateMataKuliahWithNoDosenPengampu(){
+        assertThrows(IllegalArgumentException.class, () -> {
+            MataKuliah mataKuliah = new MataKuliahBuilder()
+                    .withKode("CSCM602023 - 01.00.12.01-2020")
+                    .withNama("Pemrograman Lanjut")
+                    .withDeskripsi("Deskripsi")
+                    .build();
+        }, "Harus ada minimal 1 dosen pengampu!");
+    }
+}
