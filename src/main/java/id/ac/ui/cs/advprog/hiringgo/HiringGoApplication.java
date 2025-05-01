db700cc1b6c12a80b7367ff0cf583fc0f5964ad2
henryak25 <henryadityakosasi@gmail.com>
2025-05-01 18:06:28 +0700

    chore: mencoba setup supabase

--- src/main/java/id/ac/ui/cs/advprog/hiringgo/HiringGoApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/hiringgo/HiringGoApplication.java
@@ -1,5 +1,6 @@
 package id.ac.ui.cs.advprog.hiringgo;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HiringGoApplication {
 
     public static void main(String[] args) {
+        loadEnv();
         SpringApplication.run(HiringGoApplication.class, args);
     }
 
+
+    public static void loadEnv() {
+        Dotenv dotenv = Dotenv.load();
+
+        System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
+        System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
+        System.setProperty("DB_USER", dotenv.get("DB_USER"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+        System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+    }
 }
+
