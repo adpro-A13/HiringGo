1555b12e6b2a876c8a5d4000853f90aa20a51c36
henryak25 <henryadityakosasi@gmail.com>
2025-05-08 09:30:05 +0700

    [GREEN] adjust lowongan controller from @Controller to @RestController

--- src/main/java/id/ac/ui/cs/advprog/hiringgo/manajemenlowongan/controller/LowonganController.java
+++ src/main/java/id/ac/ui/cs/advprog/hiringgo/manajemenlowongan/controller/LowonganController.java
@@ -5,43 +5,40 @@ import id.ac.ui.cs.advprog.hiringgo.manajemenlowongan.enums.StatusLowongan;
 import id.ac.ui.cs.advprog.hiringgo.manajemenlowongan.model.Lowongan;
 import id.ac.ui.cs.advprog.hiringgo.manajemenlowongan.service.LowonganService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
-@Controller
-@RequestMapping("/lowongan")
+@RestController
+@RequestMapping("/api/lowongan")
 public class LowonganController {
 
     @Autowired
     private LowonganService lowonganService;
 
-    @GetMapping("/create")
-    public String createLowonganPage(Model model) {
-        Lowongan lowongan = new Lowongan();
-        model.addAttribute("lowongan", lowongan);
-        model.addAttribute("semesterList", Semester.values());
-        model.addAttribute("statusList", StatusLowongan.values());
-        return "manajemenlowongan/createLowongan";
+    @GetMapping
+    public List<Lowongan> getAllLowongan() {
+        return lowonganService.findAll();
     }
 
-    @PostMapping("/create")
-    public String createLowonganPost(@ModelAttribute Lowongan lowongan) {
-        lowonganService.createLowongan(lowongan);
-        return "redirect:/lowongan/list";
+    @PostMapping
+    public Lowongan createLowongan(@RequestBody Lowongan lowongan) {
+        return lowonganService.createLowongan(lowongan);
     }
 
-    @GetMapping("/list")
-    public String listLowonganPage(Model model) {
-        model.addAttribute("lowonganList", lowonganService.findAll());
-        return "manajemenlowongan/listLowongan";
+    @DeleteMapping("/{id}")
+    public void deleteLowongan(@PathVariable UUID id) {
+        lowonganService.deleteLowonganById(id);
     }
 
-    @PostMapping("/delete")
-    public String deleteLowongan(@RequestParam("id") UUID idLowongan) {
-        lowonganService.deleteLowonganById(idLowongan);
-        return "redirect:/lowongan/list";
+    @GetMapping("/semester")
+    public Semester[] getSemesterList() {
+        return Semester.values();
+    }
+
+    @GetMapping("/status")
+    public StatusLowongan[] getStatusList() {
+        return StatusLowongan.values();
     }
 }
