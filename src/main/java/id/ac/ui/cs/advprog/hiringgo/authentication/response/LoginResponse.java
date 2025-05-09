e792a4f2660d38e21f5986b1eb1a30042dfa470d
Christian Raphael Heryanto <christian.raphael@ui.ac.id>
2025-05-09 20:52:54 +0700

    [GREEN] Remove of verify endpoint & Add content of token

--- src/main/java/id/ac/ui/cs/advprog/hiringgo/authentication/response/LoginResponse.java
+++ src/main/java/id/ac/ui/cs/advprog/hiringgo/authentication/response/LoginResponse.java
@@ -1,12 +1,13 @@
 package id.ac.ui.cs.advprog.hiringgo.authentication.response;
 
 import lombok.Getter;
+import java.util.Map;
 
 @Getter
 public class LoginResponse {
     private String token;
-
     private long expiresIn;
+    private Map<String, Object> user;
 
     public String getToken() {
         return token;
@@ -23,6 +24,15 @@ public class LoginResponse {
 
     public LoginResponse setExpiresIn(long expiresIn) {
         this.expiresIn = expiresIn;
+        return this;
+    }
+    
+    public Map<String, Object> getUser() {
+        return user;
+    }
+    
+    public LoginResponse setUser(Map<String, Object> user) {
+        this.user = user;
         return this;
     }
 }
