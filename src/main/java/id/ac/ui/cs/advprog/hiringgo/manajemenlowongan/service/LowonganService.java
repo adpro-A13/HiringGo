ce296dd55c62ca6e2de7a9ae555070f7f7e0e6b7
henryak25 <henryadityakosasi@gmail.com>
2025-05-15 19:28:39 +0700

    fix: minor mistake fix

--- src/main/java/id/ac/ui/cs/advprog/hiringgo/manajemenlowongan/service/LowonganService.java
+++ src/main/java/id/ac/ui/cs/advprog/hiringgo/manajemenlowongan/service/LowonganService.java
@@ -2,7 +2,6 @@ package id.ac.ui.cs.advprog.hiringgo.manajemenlowongan.service;
 
 import id.ac.ui.cs.advprog.hiringgo.manajemenlowongan.filter.LowonganFilterStrategy;
 import id.ac.ui.cs.advprog.hiringgo.manajemenlowongan.model.Lowongan;
-import id.ac.ui.cs.advprog.hiringgo.manajemenlowongan.sort.LowonganSortStrategy;
 
 import java.util.List;
 import java.util.UUID;
